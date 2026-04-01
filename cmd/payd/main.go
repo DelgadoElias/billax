1f9637dd391759a49c680ac922efda1899956580
DelgadoElias <eliaslautarodelgado@gmail.com>
2026-03-31 22:47:11 -0300

    build: initialize Go module with required dependencies

---
+++ cmd/payd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	_ "github.com/go-chi/chi/v5"
+	_ "github.com/jackc/pgx/v5"
+	_ "github.com/google/uuid"
+	_ "golang.org/x/crypto/argon2"
+)
+
+func main() {
+	// Placeholder - TAREA 3
+}
+
---
+++ internal/config/config.go
@@ -0,0 +1,3 @@
+package config
+
+// Placeholder for TAREA 3
